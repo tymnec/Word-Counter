404f8512007072c4e47f1ab3ae935a0c84453142
Nikhil <tymnec@gmail.com>
2023-06-18 23:58:28 +0530

    :tada: simple word counter using .NET Core MVC

---
+++ Word Counter/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const countWords = () => {
+    const text = document.getElementById("input-text").value;
+    const words = text.split(" ");
+    const wordCount = words.length;
+    document.getElementById("word-count").innerHTML = `Word count: ${wordCount}`;
+}
+
+document.getElementById("count-words").addEventListener("click", countWords);
